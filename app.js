4a77d698364c3e0b7fa2dac15a504201c0a97185
J Carlos Viana F <jcvf@ic.ufal.br>
2019-03-23 21:29:12 -0300

    modularize authentication

--- app.js
+++ app.js
@@ -13,9 +13,11 @@ app.use(session({
 app.use(flash());
 
 let userController = require('./server/controller/user');
-
+let authRouter = require('./src/routers/auth');
 
 app.set('view engine', 'ejs');
+
+app.use('/', authRouter);
 
 app.get('/',function(req,res){
     if (userController.getLoggedUser() == null) {
@@ -27,46 +29,6 @@ app.get('/',function(req,res){
         });
         req.flash('success', null);
     }
-});
-
-app.get('/login', function(req, res) {
-    res.render('./auth/login.ejs', { success: req.flash('success') });
-    req.flash('success', null);
-});
-
-app.post('/login', function(req, res){
-    userController.login(req.body.login, req.body.password, function() {
-        if (userController.getLoggedUser() != null) {
-            res.redirect('/');
-        } else {
-            res.render('./auth/login.ejs', {
-                error: 'erro na autenticação',
-                success: req.flash('success')
-            });
-            req.flash('success', null);
-        }
-    });
-});
-
-app.get('/logout', function(req, res){
-    userController.logout(function() {
-        res.redirect('/');
-    });
-});
-
-app.get('/register', function(req, res) {
-    res.render('./auth/register.ejs');
-});
-
-app.post('/register', function(req, res) {
-    userController.register(req.body.name, req.body.login, req.body.password, function(errorMsg) {
-        if (errorMsg == null) {
-            req.flash('success', 'sucesso ao registrar');
-            res.redirect('/');
-        } else {
-            res.render('./auth/register.ejs', { error: errorMsg });
-        }
-    });
 });
 
 module.exports = app;
---
+++ src/routers/auth.js
@@ -0,0 +1,46 @@
+let express = require('express');
+let router = express.Router();
+
+let userController = require('./../../server/controller/user');
+
+router.route('/login')
+.get(function(req, res) {
+    res.render('./auth/login.ejs', { success: req.flash('success') });
+    req.flash('success', null);
+})
+.post(function(req, res){
+    userController.login(req.body.login, req.body.password, function() {
+        if (userController.getLoggedUser() != null) {
+            res.redirect('/');
+        } else {
+            res.render('./auth/login.ejs', {
+                error: 'erro na autenticação',
+                success: req.flash('success')
+            });
+            req.flash('success', null);
+        }
+    });
+});
+
+router.get('/logout', function(req, res){
+    userController.logout(function() {
+        res.redirect('/');
+    });
+});
+
+router.route('/register')
+.get(function(req, res) {
+    res.render('./auth/register.ejs');
+})
+.post(function(req, res) {
+    userController.register(req.body.name, req.body.login, req.body.password, function(errorMsg) {
+        if (errorMsg == null) {
+            req.flash('success', 'sucesso ao registrar');
+            res.redirect('/');
+        } else {
+            res.render('./auth/register.ejs', { error: errorMsg });
+        }
+    });
+});
+
+module.exports = router;
